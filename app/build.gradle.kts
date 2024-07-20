5031058f9f40c4740f85f89690c02e6ffe46f96c
prashant <manprashant99@gmail.com>
2024-07-20 12:54:04 +0530

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,5 +46,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.github.iammert:ReadableBottomBar:0.2")
+
 }
